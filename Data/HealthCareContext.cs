413df10bcfa1ab00204789fb040a5fd211a9fb29
jeborges <jeborgesm@hotmail.com>
2018-05-02 20:47:43 -0400

    SQL Server components and initialization.

---
+++ Data/HealthCareContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using XHealthAng5.Models;
+
+namespace XHealthAng5.Data
+{
+    public class HealthCareContext : DbContext
+    {
+        public HealthCareContext(DbContextOptions<HealthCareContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountInsurance> AccountInsurances { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<Insurance> Insurances { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<ExportHistory> ExportHistories { get; set; }
+    }
+}
